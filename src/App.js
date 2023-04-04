f3084d63c7ab86a83585159fa0fe12019c1f4514
Vadim Kaptsov <vadimkaptsov@mail.ru>
2023-04-04 10:36:56 +0300

    [Storybook] - add storybook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {Button} from "./stories/Button";
+
 function App() {
   return (
     <div>
-      app
+        <Button
+            backgroundColor="rgba(0, 94, 255, 1)"
+            label="Button"
+            onClick={() => {
+                console.log('click!')}}
+            primary
+            size="large"
+        />
     </div>
   );
 }
